4531ea12f8d8f482dc65605741243c53dd370760
root <root@raspberrypi.(none)>
2015-09-29 19:37:57 +0000

    Div/Mod Shift

--- Hmwk/Div_Mod Shift/DivModShift.s
+++ Hmwk/Div_Mod Shift/DivModShift.s
@@ -10,8 +10,36 @@
 
 	.global _start
 _start:
+	MOV R0,  #0 
 	MOV R1, #45 @Numerator
 	MOV R2,  #6 @Denominator
 	MOV R3,  #1 @Scale
 	MOV R4,  #6 @Original
 	MOV R5,  #4 @Scale
+
+_while_R4_GT_R1:
+	CMP R1, R4
+	BLT _exit
+		CMP R4, R1          @If  R4 < R1 LSL
+		BLT _lshift
+	BAL _while_R4_GT_R1 
+
+_lshift:
+	CMP R4, R1
+	BGT _rshift             @If R4 < R1 LSR and SUB
+		MOVS R4, R2, LSL R3
+		ADD R3, R3, #1      @Increment scale
+	BAL _lshift
+
+_rshift:
+	MOVS R4, R4, LSR #1     @Shift right since too big
+	SUB R1, R1, R4      
+	MOV R4, R2              @INIT denominator
+	MOV R3, #1              @Reset scale
+	MOV R0, R1              @Move numerator for output
+	B _while_R4_GT_R1   
+
+_exit:
+	MOV R7, #1
+	SWI 0
+
